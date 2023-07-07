1b30d2c29051cb5f085b4d6e5410d2f6bde3f3b2
unknown <tomekf.dev@gmail.com>
2023-07-07 18:45:04 +0200

    core app urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 
+app_name = "core"
 urlpatterns = [
-    path("", views.index_page_view, name="index_page"),
+    path("", views.index_page_view, name="index"),
+    path("shop/", views.shop_page_view, name="shop"),
+    path("about/", views.about_page_view, name="about"),
+    path("contact/", views.contact_page_view, name="contact"),
+    path("vendor/", views.vendor_page_view, name="vendor"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index_page_view(request):
     return render(request, "core/index.html")
+
+
+def shop_page_view(request):
+    return render(request, "core/product_list.html")
+
+
+def about_page_view(request):
+    return render(request, "core/about.html")
+
+
+def contact_page_view(request):
+    return render(request, "core/contact.html")
+
+
+def vendor_page_view(request):
+    return render(request, "core/vendor.html")
