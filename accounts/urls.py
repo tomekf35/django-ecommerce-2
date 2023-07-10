2be0c1e46bc64f4f42d1e700f38b2115237c73f5
unknown <tomekf.dev@gmail.com>
2023-07-10 11:25:53 +0200

    section highlight in navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView
 
 app_name = "accounts"
 urlpatterns = []
--- core/views.py
+++ core/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 
 
 def index_page_view(request):
-    return render(request, "core/index.html")
+    section = "index"
+    return render(request, "core/index.html", {"section": section})
 
 
 def shop_page_view(request):
-    return render(request, "core/product_list.html")
+    section = "shop"
+    return render(request, "core/product_list.html", {"section": section})
 
 
 def about_page_view(request):
-    return render(request, "core/about.html")
+    section = "about"
+    return render(request, "core/about.html", {"section": section})
 
 
 def contact_page_view(request):
-    return render(request, "core/contact.html")
+    section = "contact"
+    return render(request, "core/contact.html", {"section": section})
 
 
 def vendor_page_view(request):
-    return render(request, "core/vendor.html")
+    section = "vendor"
+    return render(request, "core/vendor.html", {"section": section})
